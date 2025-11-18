6c8fc2c252f2f95d756ac2214c6f1e9b2ea8a160
José Revelo <josefernandorevelo@gmail.com>
2025-11-18 22:45:43 +0000

    Updated index.jsp with new content

--- src/main/java/com/nextwork/app/App.java
+++ src/main/java/com/nextwork/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "If you see this line in Github, that means your latest changes are getting pushed to your cloud repo :o" );
     }
 }
